67052cd6710dcca02aeb54f3153d11d25d38be05
nsemirdev <nsemirdev@gmail.com>
2022-12-13 18:20:50 +0300

    configure GFTextField

---
+++ GitHub-Followers/CustomViews/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField.swift
+//  GitHub-Followers
+//
+//  Created by Emir Alkal on 13.12.2022.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = .preferredFont(forTextStyle: .title2)
+        
+        adjustsFontSizeToFitWidth = true // if someone is really huge username font will be shrink
+        minimumFontSize = 12             // shrink maximum 12pt.
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a username"
+    }
+
+}
