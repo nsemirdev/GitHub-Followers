2402e0dd8eee27081523b45745d5c556989fd8e2
nsemirdev <nsemirdev@gmail.com>
2023-01-23 23:28:27 +0300

    starter xcode project

---
+++ GitHub-Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub-Followers
+//
+//  Created by Emir Alkal on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
