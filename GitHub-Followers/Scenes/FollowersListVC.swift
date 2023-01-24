0f6b7e2813978c496c4d7b6788b95f0b10e970e1
nsemirdev <nsemirdev@gmail.com>
2023-01-24 20:51:33 +0300

    bug fixes [update followers list on cancel button clicked]

--- GitHub-Followers/Scenes/FollowersListVC.swift
+++ GitHub-Followers/Scenes/FollowersListVC.swift
@@ -124,6 +124,7 @@ extension FollowersListVC: FollowersShowable {}
 extension FollowersListVC: UISearchResultsUpdating {
   func updateSearchResults(for searchController: UISearchController) {
     guard let filter = searchController.searchBar.text, !filter.isEmpty else {
+      updateData(on: followers)
       return
     }
     filteredFollowers = followers.filter {
